3d36eae181195a0c94b6de60487cae38c5d60d8c
liana16 <liana_you@bk.ru>
2022-12-20 12:43:32 +0300

    Задача 67

---
+++ task67/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 67: Напишите программу, которая будет принимать
+//  на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+int DataSum(int number)
+{
+    if (number > 0) 
+    {
+         return number%10 + DataSum(number/10);
+    }
+    return 0;
+}
+
+
+Console.WriteLine("введи число n: ");
+int n = int.Parse(Console.ReadLine());
+int result = DataSum(n);
+Console.WriteLine($"Сумма цифр числа n равна: {result} ");
